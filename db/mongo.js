606d7230acda60f6894a4890a957d536b330af0e
Geoffrey <geoffrey.papaux@gmail.com>
2013-04-03 15:14:39 +0200

    creating empty files for database implementations

---
+++ db/mongo.js
@@ -0,0 +1,11 @@
+/**********************************************************
+ * This file is part of the Gitification project
+ *
+ * Authors	:	Vincent Pasquier, Vincent Grivel
+ *						Dorian Gambin, Geoffrey Papaux
+ *
+ * Purpose :	Implementation of the database functions using
+ *						querying the mongo db
+ *
+ *********************************************************/
+'use strict';
